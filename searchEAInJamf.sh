a12f00dce56eea382228c60398200961c2522514
Laurent Pertois <laurent+gh@pertois.fr>
2022-01-28 16:35:45 +0100

    Extension attribute search
    Single script for EA or combined script for scripts and EAs

---
+++ searchEAInJamf.sh
@@ -0,0 +1,109 @@
+#!/bin/bash
+
+# This script is used on an admin computer to search in Jamf Pro extension attributes which of them use a specific command.
+# It's using the Classic API to search.
+# We recommend to have a dedicated user for that search.
+# The script does the following:
+# 	List all extension attributes ID
+# 	Go through each ID, get the <script> content
+# 	Search the string
+# 	If contains string, get <name>
+# 	Report with extension attributes ID and <name>
+# 	Report with URL to the extension attributes in Jamf Pro
+#	Report lines containing the searched value
+
+# Please change the variables according to your needs
+serverURL="https://foo.jamfcloud.com"       # i.e.: https://server.domain.tld:port or https://instance.jamfcloud.com
+userName="foo"                      # it is recommended to create a dedicated read-only user that has read-only access to scripts
+userPasswd="foo"
+
+# Check if something is passed as an argument. If nothing is passed, search for python
+if [[ "$1" == "" ]]; then
+    searchString="python"
+else
+    searchString="$1"
+fi
+
+
+# Get a list of all the extension attributes
+allEAs=$(curl -s -X GET "$serverURL/JSSResource/computerextensionattributes" -H "accept: application/xml" -u "$userName":"$userPasswd")
+
+if [[ $(echo "$allEAs" | grep -ic "xml") -eq 0 ]] ; then
+    echo "It seems we cannot get data from your server, please check the variables in your script or the credentials used"
+    exit
+fi
+
+# XSLT to transform the XML make a list of extension attributes IDs
+XSLT='<?xml version="1.0" encoding="UTF-8"?>
+<xsl:stylesheet version="1.0" xmlns:xsl="http://www.w3.org/1999/XSL/Transform">
+<xsl:output method="text"/>
+<xsl:template match="/">
+<xsl:for-each select="computer_extension_attributes/computer_extension_attribute">
+<xsl:value-of select="id"/>
+<xsl:text>&#xa;</xsl:text>
+</xsl:for-each>
+</xsl:template>
+</xsl:stylesheet>'
+
+allEAsID=$(echo "$allEAs" | xsltproc <(echo "$XSLT") -)
+
+countEAs=$(echo "$allEAs" | xmllint --xpath '/computer_extension_attributes/size/text()' -)
+
+# If we have 0 extension attribute, either there is an issue or we don't need that script
+# If more than 0, get the correct plural version if needed
+if [[ "$countEAs" == 0 ]]; then
+    echo "You don't have any extension attributes in your Jamf Pro instance or we cannot connect, good bye"
+    exit 0
+elif [[ "$countEAs" == 1 ]]; then
+    countEAsName="extension attribute"
+else
+    countEAsName="extension attributes"
+fi
+
+# Inform you
+echo "You have $countEAs $countEAsName in your instance of Jamf Pro"
+echo "We are looking for: $searchString"
+echo ""
+
+while read -r extensionAttributeID; do
+    
+    # Get the full content of the extension attribute
+    extensionAttributeFullInfo=$(curl -s -X GET "$serverURL/JSSResource/computerextensionattributes/id/$extensionAttributeID" -u "$userName":"$userPasswd")
+    
+    # Get the decoded version of the script itself
+    extensionAttributeContentDecoded=$(echo "$extensionAttributeFullInfo" | xmllint --xpath 'string(//computer_extension_attribute/input_type/script)' -)
+    
+    # Decode the script and search for the number of occurrences of the command
+    extensionAttributeContentSearch=$(echo "$extensionAttributeContentDecoded" | grep -c "$searchString")
+    
+    # If there is at least 1 occurrences of the command, let's go
+    if [[ "$extensionAttributeContentSearch" -gt 0 ]]; then
+        
+        # Get the name of the script
+        extensionAttributeName=$(echo "$extensionAttributeFullInfo" | xmllint --xpath 'string(//computer_extension_attribute/name)' -)
+        
+        # Get line numbers showing the searched string, all in one line, separated with spaces
+        extensionAttributeLineNumbers=$(echo "$extensionAttributeContentDecoded" | grep -n "$searchString" | awk -F":" '{ print $1 }' | tr '\n' ' ')
+        
+        # If more than 0, get the correct plural version if needed
+        if [[ "$extensionAttributeContentSearch" == 1 ]]; then
+            occurenceName="occurrence"
+            lineNumbersName="Line that has"
+        else
+            occurenceName="occurrences"
+            lineNumbersName="Lines that have"
+        fi
+        
+        # Let's tell you what we found
+        echo "The script called \"$extensionAttributeName\" contains $extensionAttributeContentSearch $occurenceName of \"$searchString\""
+        echo "Script ID is: $extensionAttributeID"
+        echo "Script URL is: $serverURL/computerExtensionAttributes.html?id=$extensionAttributeID"
+        echo "$lineNumbersName \"$searchString\": $extensionAttributeLineNumbers"
+        echo ""
+        
+    fi
+    
+done <<< "$allEAsID"
+
+echo ""
+echo "Search is finished, happy $countEAsName reviewing"
---
+++ searchScriptsAndEAsInJamf.sh
@@ -0,0 +1,195 @@
+#!/bin/bash
+
+# This script is used on an admin computer to search in Jamf Pro scripts and extension attributes which of them use a specific command.
+# It's using the Classic API to search.
+# We recommend to have a dedicated user for that search.
+# The script does the following:
+# 	List all scripts/extension attributes ID
+# 	Go through each ID, get the <script_contents_encoded> or <script> for the extension attributes
+# 	Decode the <script_contents_encoded> (not needed for extension attributes)
+# 	Search the string
+# 	If contains string, get <name>
+# 	Report with script/extension attributes ID and <name>
+# 	Report with URL to the script/extension attributes in Jamf Pro
+#	Report lines containing the searched value
+
+# Please change the variables according to your needs
+serverURL="https://foo.jamfcloud.com"       # i.e.: https://server.domain.tld:port or https://instance.jamfcloud.com
+userName="foo"                      # it is recommended to create a dedicated read-only user that has read-only access to scripts
+userPasswd="foo"
+
+# Check if something is passed as an argument. If nothing is passed, search for python
+if [[ "$1" == "" ]]; then
+    searchString="python"
+else
+    searchString="$1"
+fi
+
+### Scripts ###
+# Get a list of all the scripts
+allScripts=$(curl -s -X GET "$serverURL/JSSResource/scripts" -H "accept: application/xml" -u "$userName":"$userPasswd")
+
+if [[ $(echo "$allScripts" | grep -ic "xml") -eq 0 ]] ; then
+    echo "It seems we cannot get data from your server, please check the variables in your script or the credentials used"
+    exit
+fi
+
+# XSLT to transform the XML make a list of script IDs
+XSLT='<?xml version="1.0" encoding="UTF-8"?>
+<xsl:stylesheet version="1.0" xmlns:xsl="http://www.w3.org/1999/XSL/Transform">
+<xsl:output method="text"/>
+<xsl:template match="/">
+<xsl:for-each select="scripts/script">
+<xsl:value-of select="id"/>
+<xsl:text>&#xa;</xsl:text>
+</xsl:for-each>
+</xsl:template>
+</xsl:stylesheet>'
+
+allScriptsID=$(echo "$allScripts" | xsltproc <(echo "$XSLT") -)
+
+countScripts=$(echo "$allScripts" | xmllint --xpath '/scripts/size/text()' -)
+
+# If we have 0 script, either there is an issue or we don't need that script
+# If more than 0, get the correct plural version if needed
+if [[ "$countScripts" == 0 ]]; then
+    echo "You don't have any scripts in your Jamf Pro instance or we cannot connect, good bye"
+    exit 0
+elif [[ "$countScripts" == 1 ]]; then
+    countScriptsName="script"
+else
+    countScriptsName="scripts"
+fi
+
+# Inform you
+echo "You have $countScripts $countScriptsName in your instance of Jamf Pro"
+echo "We are looking for: $searchString"
+echo ""
+
+while read -r scriptID; do
+    
+    # Get the full content of the script
+    scriptFullInfo=$(curl -s -X GET "$serverURL/JSSResource/scripts/id/$scriptID" -u "$userName":"$userPasswd")
+    
+    # Get the decoded version of the script itself
+    scriptContentDecoded=$(echo "$scriptFullInfo" | xmllint --xpath 'string(//script/script_contents_encoded)' - | base64 -d)
+    
+    # Decode the script and search for the number of occurrences of the command
+    contentSearch=$(echo "$scriptContentDecoded" | grep -c "$searchString")
+    
+    # If there is at least 1 occurrences of the command, let's go
+    if [[ "$contentSearch" -gt 0 ]]; then
+        
+        # Get the name of the script
+        scriptName=$(echo "$scriptFullInfo" | xmllint --xpath 'string(//script/name)' -)
+        
+        # Get line numbers showing the searched string, all in one line, separated with spaces
+        lineNumbers=$(echo "$scriptContentDecoded" | grep -n "$searchString" | awk -F":" '{ print $1 }' | tr '\n' ' ')
+        
+        # If more than 0, get the correct plural version if needed
+        if [[ "$contentSearch" == 1 ]]; then
+            occurenceName="occurrence"
+            lineNumbersName="Line that has"
+        else
+            occurenceName="occurrences"
+            lineNumbersName="Lines that have"
+        fi
+        
+        # Let's tell you what we found
+        echo "The script called \"$scriptName\" contains $contentSearch $occurenceName of \"$searchString\""
+        echo "Script ID is: $scriptID"
+        echo "Script URL is: $serverURL/view/settings/computer/scripts/$scriptID"
+        echo "$lineNumbersName \"$searchString\": $lineNumbers"
+        echo ""
+        
+    fi
+    
+done <<< "$allScriptsID"
+
+echo ""
+#echo "Search is finished, happy $countScriptsName reviewing"
+
+
+### Extension Attributes ###
+# Get a list of all the extension attributes
+allEAs=$(curl -s -X GET "$serverURL/JSSResource/computerextensionattributes" -H "accept: application/xml" -u "$userName":"$userPasswd")
+
+if [[ $(echo "$allEAs" | grep -ic "xml") -eq 0 ]] ; then
+    echo "It seems we cannot get data from your server, please check the variables in your script or the credentials used"
+    exit
+fi
+
+# XSLT to transform the XML make a list of extension attributes IDs
+XSLT='<?xml version="1.0" encoding="UTF-8"?>
+<xsl:stylesheet version="1.0" xmlns:xsl="http://www.w3.org/1999/XSL/Transform">
+<xsl:output method="text"/>
+<xsl:template match="/">
+<xsl:for-each select="computer_extension_attributes/computer_extension_attribute">
+<xsl:value-of select="id"/>
+<xsl:text>&#xa;</xsl:text>
+</xsl:for-each>
+</xsl:template>
+</xsl:stylesheet>'
+
+allEAsID=$(echo "$allEAs" | xsltproc <(echo "$XSLT") -)
+
+countEAs=$(echo "$allEAs" | xmllint --xpath '/computer_extension_attributes/size/text()' -)
+
+# If we have 0 extension attribute, either there is an issue or we don't need that script
+# If more than 0, get the correct plural version if needed
+if [[ "$countEAs" == 0 ]]; then
+    echo "You don't have any extension attributes in your Jamf Pro instance or we cannot connect, good bye"
+    exit 0
+elif [[ "$countEAs" == 1 ]]; then
+    countEAsName="extension attribute"
+else
+    countEAsName="extension attributes"
+fi
+
+# Inform you
+echo "You have $countEAs $countEAsName in your instance of Jamf Pro"
+echo "We are looking for: $searchString"
+echo ""
+
+while read -r extensionAttributeID; do
+    
+    # Get the full content of the extension attribute
+    extensionAttributeFullInfo=$(curl -s -X GET "$serverURL/JSSResource/computerextensionattributes/id/$extensionAttributeID" -u "$userName":"$userPasswd")
+    
+    # Get the decoded version of the script itself
+    extensionAttributeContentDecoded=$(echo "$extensionAttributeFullInfo" | xmllint --xpath 'string(//computer_extension_attribute/input_type/script)' -)
+    
+    # Decode the script and search for the number of occurrences of the command
+    extensionAttributeContentSearch=$(echo "$extensionAttributeContentDecoded" | grep -c "$searchString")
+    
+    # If there is at least 1 occurrences of the command, let's go
+    if [[ "$extensionAttributeContentSearch" -gt 0 ]]; then
+        
+        # Get the name of the script
+        extensionAttributeName=$(echo "$extensionAttributeFullInfo" | xmllint --xpath 'string(//computer_extension_attribute/name)' -)
+        
+        # Get line numbers showing the searched string, all in one line, separated with spaces
+        extensionAttributeLineNumbers=$(echo "$extensionAttributeContentDecoded" | grep -n "$searchString" | awk -F":" '{ print $1 }' | tr '\n' ' ')
+        
+        # If more than 0, get the correct plural version if needed
+        if [[ "$extensionAttributeContentSearch" == 1 ]]; then
+            occurenceName="occurrence"
+            lineNumbersName="Line that has"
+        else
+            occurenceName="occurrences"
+            lineNumbersName="Lines that have"
+        fi
+        
+        # Let's tell you what we found
+        echo "The script called \"$extensionAttributeName\" contains $extensionAttributeContentSearch $occurenceName of \"$searchString\""
+        echo "Script ID is: $extensionAttributeID"
+        echo "Script URL is: $serverURL/computerExtensionAttributes.html?id=$extensionAttributeID"
+        echo "$lineNumbersName \"$searchString\": $extensionAttributeLineNumbers"
+        echo ""
+        
+    fi
+    
+done <<< "$allEAsID"
+
+echo ""
+echo "Search is finished, happy $countScriptsName and $countEAsName reviewing"
